931eeb7d22b0e931ffadc55a028bd2114bc653c9
c43my <108557452+c43my@users.noreply.github.com>
2022-07-04 15:21:11 -0500

    uptade002
    It is planned to name everything in English. Fixed connections for better performance, also comment errors.

--- Sys_Seguridad.ino
+++ Sys_Seguridad.ino
@@ -1,140 +1,133 @@
 #include <LiquidCrystal.h>
 #include <Keypad.h>
 
-LiquidCrystal lcd {0,1,5,4,3,2};//RS EV D5 D6 D7 D8
+//Initialize LiquidCrystal
+LiquidCrystal lcd {0,1,5,4,3,2};//Param: RS, EV, D4, D5, D6, D7.
 
-const byte ROWS = 4; //four rows
-const byte COLS = 3; //three columns
+//Initialize Keypad
+#define ROWS 4
+#define COLS 3
 char keys[ROWS][COLS] = {
   {'1','2','3'},
   {'4','5','6'},
   {'7','8','9'},
   {'*','0','#'}
 };
-byte rowPins[ROWS] = {12, 11, 10, 9}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {8, 7, 6}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {23, 25, 27, 29}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {22, 24, 26}; //connect to the column pinouts of the keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-//VARIABLES GLOBALES
-
-//Conexiones de pines para el led 
-int redPin = 32;
-int greenPin = 34;
-int bluePin = 36;
-
-//Variables globales para el sistema de seguridad
+//Initialize SYSTEM SECURITY variables
 int pos=0;
-int atmps = 1;
-
-//Contrasenia del sistema
+int attempt = 0;
+//Password
 char secretCode[7] = {'8','7','6','1','8','9','#'};
 char enterCode[7]={'0','0','0','0','0','0','#'};
 
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+//Pin connection for RGB LED (Always in the POWER field)
+#define REDPIN 11
+#define GREENPIN 12
+#define BLUEPIN 13
 
 void setup(){
-    pinMode(redPin, OUTPUT);
-    pinMode(greenPin, OUTPUT);
-    pinMode(bluePin, OUTPUT);
-    
-    lcd.begin(16, 2);
-    lcdSetPrint(0,0,"   Welcome!  ");
-    delay(3000);
-    lcd.clear();
+  //Initialize RGB LED
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
+  
+  lcd.begin(16, 2);
+  lcdSetPrint(0,0,"   Welcome!  ");
+  delay(3000);
+  lcd.clear();
 }
-
 void loop(){
-    sistemaSeguridad();
+    securitySystem();
 }
-
-void sistemaSeguridad(){
-    if(atmps < 4){ 
-        ingresoContrasenia();
+void securitySystem(){
+    if(attempt < 3){ 
+        enterPassword();
     }
     else{
-        sistemaBloqueado();
+        lockSystem();
     }
 }
-
-void ingresoContrasenia(){
+void enterPassword(){
   lcdSetPrint(0,0,"PASSWORD:");
   char key = keypad.getKey();
   if(key){
     if(key=='#'||pos==7){
-      verificar(enterCode);
+      check();
     }else{
-      leerContrasenia(key); 
+      readPassword(key); 
     }
   }  
 }
-void leerContrasenia(char key){
+void readPassword(char key){
   lcdSetPrint(pos,1,"*");
   enterCode[pos]=key;
   pos++;
 }
-void verificar(char charac[]){
+void check(){
     for(pos=0;pos<7;pos++){
-      if(charac[pos]!=secretCode[pos]){
-        contraseniaIncorrecta();
+      if(enterCode[pos]!=secretCode[pos]){
+        wrongPassword();
         return;
       }
     }
-    contraseniaCorrecta();
+    correctPassword();
 }
-void contraseniaIncorrecta(){
-  lcdSetPrint(0,1,"INCORRECTA!");
+void wrongPassword(){
+  lcdSetPrint(0,1,"WRONG!");
   enterCode[6]='#';
-  ledCon(255,120,0,2000); //Led en naranja y espera 2 segundos
-  if(atmps<3){
+  ledWth(255,120,0,2000); //Orange and wait 2 seconds.
+  if(attempt<2){
     lcdSetPrint(0,1," TRY AGAIN!");
-    ledCon(0,0,0,2000); //Led sin encender y...
+    ledWth(0,0,0,2000); //Led turn off...
     }
   lcd.clear();
   pos = 0;
-  atmps++;
+  attempt++;
 }
-
-void contraseniaCorrecta(){
-  lcdSetPrint(0,1,"CORRECTO");
+void correctPassword(){
+  lcdSetPrint(0,1,"CORRECT...");
   delay(1500);
   lcd.clear();
-  lcdSetPrint(0,0,"BIENVENIDO");
-  ledCon(0,255,0,2000);  //Verde
-  ledCon(0,0,0,2000);
-  sistemaDesbloqueado();
+  lcdSetPrint(0,0,"   WELCOME");
+  ledWth(0,255,0,2000);  //Green
+  ledWth(0,0,0,2000);
+  unlockSystem();
   lcd.clear();
 }
-void sistemaBloqueado(){
-  lcdSetPrint(0,0," SYS BLOQUEADO");
-  ledCon(255,0,0,5000);  //Rojo
-  ledCon(0,0,0,2000);
-  pos = 0;
-  atmps = 0;
+void lockSystem(){
+  lcdSetPrint(0,0,"SYSTEM LOCKED");
+  ledWth(255,0,0,5000);  //Red
+  ledWth(0,0,0,2000);
+  pos = attempt = 0;
   lcd.clear();
 }
-void sistemaDesbloqueado(){
-    ledCon(120,0,255,1000);
-    ledCon(255,0,120,1000);
-    ledCon(255,0,255,1000);
-    ledCon(255,0,120,1000);
-    ledCon(255,0,255,1000);
-    ledCon(255,0,120,1000);
-    ledCon(120,0,255,1000);
-    ledCon(255,0,255,1000);
-    ledCon(255,0,120,1000);
-    ledCon(120,0,255,1000);
-    pos=0;
-    atmps=1;
+void unlockSystem(){
+    ledWth(120,0,255,1000);
+    ledWth(255,0,120,1000);
+    ledWth(255,0,255,1000);
+    ledWth(255,0,120,1000);
+    ledWth(255,0,255,1000);
+    ledWth(255,0,120,1000);
+    ledWth(120,0,255,1000);
+    ledWth(255,0,255,1000);
+    ledWth(255,0,120,1000);
+    ledWth(120,0,255,1000);
+    pos=attempt=0;
 }
 void setColor(int red, int green, int blue){
-  analogWrite(redPin,red);
-  analogWrite(greenPin,green);
-  analogWrite(bluePin,blue);
+  analogWrite(REDPIN,red);
+  analogWrite(GREENPIN,green);
+  analogWrite(BLUEPIN,blue);
 }
-void ledCon(int r, int g, int b, int time_ms){
+void ledWth(int r, int g, int b, int time_ms){
   setColor(r,g,b);
   delay(time_ms);
 }
-void lcdSetPrint(int c, int f, char mensaje[]){
+void lcdSetPrint(int c, int f, char message[]){
   lcd.setCursor(c, f);
-  lcd.print(mensaje);
+  lcd.print(message);
 }
